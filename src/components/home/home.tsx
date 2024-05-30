523d98bb175395abd4bf613d8faf5d9a332afb28
apreswilson <apreswilson@gmail.com>
2024-05-30 13:11:32 -0400

    update home.tsx

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,6 +1,17 @@
+import { useNavigate } from "react-router-dom"
+
 function Home() {
+  const navigate = useNavigate();
+
+  const navigateToPage = () => {
+    navigate("/calendar/home");
+  }
   return (
-    <p>hi</p>
+    <>
+      <p>hi</p>
+      <button onClick={navigateToPage}>Test</button>
+    </>
+
   )
 }
 
