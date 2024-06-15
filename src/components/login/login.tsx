397801b62d4dd64ba0d47aab72a628c0c82db78e
apreswilson <apreswilson@gmail.com>
2024-06-15 08:40:31 -0400

    massive css implementation

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,23 +1,39 @@
-import { useNavigate } from "react-router-dom"
-import { useState } from "react";
+// components/Login.tsx
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../context/authContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
+import './login.css';
 
-function Login() {
+
+const Login: React.FC = () => {
   const navigate = useNavigate();
-  const [status, setStatus] = useState("");
+  const { login, isAuthenticated } = useAuth();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [status, setStatus] = useState('');
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/calendar/home');
+    }
+  }, [isAuthenticated, navigate]); // Depend on isAuthenticated and navigate
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    // ! This is only a temporary form of login that will be replaced later
     const getSignupInfo = localStorage.getItem("User")?.split(",") || [];
-    const [registeredEmail, registeredPassword] = getSignupInfo;
+    const [, registeredEmail, registeredPassword] = getSignupInfo;
     const getFormInput = new FormData(event.currentTarget);
     const email = getFormInput.get("email") as string;
     const password = getFormInput.get("password") as string;
 
     if (registeredEmail === email && registeredPassword === password) {
-      setStatus("Logging In");
+      setStatus("Logging In...");
       setTimeout(() => {
+        login();
         navigate("/calendar/home");
       }, 3000);
     } else if (getSignupInfo.length < 1) {
@@ -25,24 +41,41 @@ function Login() {
     } else {
       setStatus("Incorrect Credentials");
     }
-
-  }
+  };
 
   return (
     <div className="login-layout">
-      <form className="login-form" onSubmit={handleSubmit}>
-        <h1 className="form-title">Login</h1>
-        <input type="text" name="email" placeholder="Email"></input>
-        <input type="password" name="password" placeholder="Password"></input>
-        <button type="submit" className="submit">Login</button>
-        <p>Dont have an account?</p>
-        <button onClick={() => navigate("/calendar/signup")}>Register</button>
-        <span className={status === "Logging In" ? "success" : "fail"}>{status}</span>
+      <form className="page-form login-form" onSubmit={handleSubmit}>
+        <h1 className="form-title">Sign In</h1>
+        <div className='form-input input-field'>
+          <FontAwesomeIcon icon={faUser} size='2xl'></FontAwesomeIcon>
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            value={email}
+            autoComplete='off'
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className='form-input input-field'>
+          <FontAwesomeIcon icon={faLock} size='2xl'></FontAwesomeIcon>
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={password}
+            autoComplete='off'
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="form-button submit">Login</button>
+        <p>Don't have an account?</p>
+        <button type="button" className="form-button register" onClick={() => navigate('/calendar/signup')}>Sign Up</button>
+        <span id="status" className={status === 'Logging In...' ? 'success' : 'fail'}>{status}</span>
       </form>
-
     </div>
-
-  )
-}
+  );
+};
 
 export default Login;
--- src/components/signup/signup.tsx
+++ src/components/signup/signup.tsx
@@ -1,9 +1,14 @@
 import { useState, ChangeEvent } from "react";
 import React from "react";
 import { useNavigate } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faCheckCircle, faAddressBook } from "@fortawesome/free-regular-svg-icons";
+import { faLock } from "@fortawesome/free-solid-svg-icons";
+import './signup.css';
 
 function SignUp() {
   const [formData, setFormData] = useState({
+    name: "",
     email: "",
     password: "",
     confirmPassword: ""
@@ -11,10 +16,12 @@ function SignUp() {
   const [status, setStatus] = useState("");
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   const passwordRegex = /^(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$/;
+  const getSignupInfo = localStorage.getItem("User")?.split(",") || [];
 
   const isPasswordMatch = formData.password === formData.confirmPassword && formData.confirmPassword !== "";
   const isEmailValid = emailRegex.test(formData.email);
   const isPasswordValid = passwordRegex.test(formData.confirmPassword)
+  const isFirstPassValid = passwordRegex.test(formData.password);
 
   const navigate = useNavigate();
 
@@ -31,8 +38,12 @@ function SignUp() {
       return;
     }
 
-    //! Account information using localstorage is only temporary    
-    const setAccountInfo = `${formData.email},${formData.confirmPassword}`;
+    if (formData.email === getSignupInfo[0]) {
+      setStatus("Email Already Exists");
+      return;
+    }
+
+    const setAccountInfo = `${formData.name},${formData.email},${formData.confirmPassword}`;
     localStorage.setItem("User", setAccountInfo)
 
     setStatus("Account Created Successfully");
@@ -51,42 +62,83 @@ function SignUp() {
 
   return (
     <div className="signup-layout">
-      <form className="signup-form" onSubmit={handleSubmit}>
-        <h1 className="form-title">Register</h1>
-        <input
-          type="text"
-          name="email"
-          placeholder="Email Address"
-          onChange={handleChange}
-          value={formData.email}
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Create Password"
-          onChange={handleChange}
-          value={formData.password}
-          required
-        />
-        <input
-          type="password"
-          name="confirmPassword"
-          placeholder="Confirm Password"
-          onChange={handleChange}
-          value={formData.confirmPassword}
-          className={isPasswordMatch ? "" : "border-red"}
-          required
-        />
-        <p>Password Requirements:</p>
-        <ul>
-          <li>No less than 8 characters in length</li>
-          <li>Must contain a special character</li>
-          <li>Must contain 1 capital letter</li>
-          <li>No Spaces</li>
-        </ul>
-        <button type="submit" disabled={!isPasswordMatch}>Register</button>
-        <span className={status === "Account Created Successfully" ? "success" : "fail"}>{status}</span>
+      <form className="page-form signup-form" onSubmit={handleSubmit}>
+        <h1 className="form-title">Sign Up</h1>
+        <div className="form-input input-field">
+          <FontAwesomeIcon icon={faAddressBook} size='2xl'></FontAwesomeIcon>
+          <input
+            type="text"
+            name="name"
+            placeholder="First Name"
+            onChange={handleChange}
+            value={formData.name}
+            autoComplete="off"
+            required
+          />
+        </div>
+
+        <div className="form-input input-field">
+          <FontAwesomeIcon icon={faEnvelope} size='2xl'></FontAwesomeIcon>
+          <input
+            type="text"
+            name="email"
+            placeholder="Email Address"
+            onChange={handleChange}
+            value={formData.email}
+            autoComplete="off"
+            required
+          />
+        </div>
+        <div className="form-input input-field" id={isPasswordMatch ? "border-green" : "border-red"}>
+          <FontAwesomeIcon icon={faLock} size='2xl'></FontAwesomeIcon>
+          <input
+            type="password"
+            name="password"
+            placeholder="Create Password"
+            onChange={handleChange}
+            value={formData.password}
+            autoComplete="off"
+            required
+          />
+        </div>
+        <div className="form-input input-field" id={isPasswordMatch ? "border-green" : "border-red"}>
+          <FontAwesomeIcon icon={faCheckCircle} size='2xl'></FontAwesomeIcon>
+          <input
+            type="password"
+            name="confirmPassword"
+            placeholder="Confirm Password"
+            onChange={handleChange}
+            value={formData.confirmPassword}
+            autoComplete="off"
+            required
+          />
+        </div>
+        {isEmailValid ?
+          <></>
+          :
+          <div className="requirements">
+            <p>Username requiements:</p>
+            <ul>
+              <li>Username must be a valid email</li>
+            </ul>
+          </div>
+        }
+        {isFirstPassValid ?
+          <></>
+          :
+          <div className="requirements">
+            <p>Password Requirements:</p>
+            <ul>
+              <li>No less than 8 characters in length</li>
+              <li>Must contain a special character</li>
+              <li>Must contain 1 capital letter</li>
+              <li>No Spaces</li>
+            </ul>
+          </div>
+        }
+
+        <button type="submit" className="form-button" id={isPasswordMatch && isEmailValid ? "matched" : "not-match"} disabled={!isPasswordMatch || !isEmailValid}>Register</button>
+        <span className={status === "Account Created Successfully" ? "success" : "fail"} id="status">{status}</span>
       </form>
     </div>
   );
